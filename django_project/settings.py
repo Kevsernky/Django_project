75a898124fced099a9faaf58d74a5fa8d37a3f45
GOZEN <gozen@GOZEN-MacBook-Pro.local>
2025-08-25 12:00:17 +0300

    SET-POSTRESQL-DATABASE setup was done with docker

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,7 +30,6 @@ ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
-#yorum satırı
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'local_project',
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': 'host.docker.internal',
+        'PORT': 5434,
+        'CONN_MAX_AGE': os.environ.get('CONN_MAX_AGE', 300),
     }
 }
 
